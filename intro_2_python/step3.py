8157c2cb1695ef80f733852e5449fdeced41c24e
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2024-05-13 19:55:25 -0700

    Create step3.py

---
+++ intro_2_python/step3.py
@@ -0,0 +1,13 @@
+#Name return value from a funtion
+def welcome(name, location):
+    print("Hi", name, "welcome to", location)
+welcome('Erik', 'this tutorial')
+
+print("\n----------\n")
+
+#obtain and return a value to a function
+def add(a,b):
+    return a+ b
+
+result = add(4, 8) #assign vaule of 4 & 8 to a & b
+print (result)
