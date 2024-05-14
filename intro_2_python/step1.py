d6cbfcf05d1bf8de174e89ba82b3baa25a225675
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2024-05-13 19:54:19 -0700

    Create step1.py

---
+++ intro_2_python/step1.py
@@ -0,0 +1,26 @@
+#Set 1, Basic variables and display them
+myString1 = "test 1 "
+myString2 = "test 2"
+print(myString1 + myString2)
+
+print("\n----------\n")
+
+#Set 1a, learning about \
+myString3 = "\"test\'s 1\""
+print(myString3)
+
+print("\n----------\n")
+
+#Set 1b, Multiple lines
+myBook1 = ("Hi, it's time to write\n "
+           "a few lines. test 2\r")
+print(myBook1)
+
+print("\n----------\n")
+
+#Set 1c, if/else based on variable assignment - IF ELSE STATEMENT
+doorLocked = False
+if doorLocked:
+    print("hey door is locked")
+else:
+    print('Door is open')
