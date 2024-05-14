d79c3bcb8d56ed29a04a8803a0e35f9932f27027
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2024-05-13 19:54:57 -0700

    Create step2.py

---
+++ intro_2_python/step2.py
@@ -0,0 +1,32 @@
+#Set 2, print repeat single line - FOR LOOP
+for holder in 'SlimShady':
+    print(holder)
+
+print("\n----------\n")
+
+#Set 3, Create list - SELECTIVE FOR LOOP
+myList = ['apple', 2, 'ban', 'straw']
+for thingie in myList:
+    print(thingie)
+    print(myList[3])
+
+print("\n----------\n")
+
+#Set 4, While whatever is TRUE LOOP
+r = 1
+while r <= 4:
+    print(r, 'ya boy')
+    r = r + 1
+
+print("\n----------\n")
+
+#Set 5, count characters define function
+name = len('RyanJosephBridglal')
+print(name)
+
+print("\n----------\n")
+
+#define hi to the words, display it
+def hi():
+    print('hi f er wassup')
+hi()
