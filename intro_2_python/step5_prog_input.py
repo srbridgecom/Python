26eeb024f30a70db876a259350836838842f29e5
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2024-05-16 10:20:20 -0700

    Update step5_prog_input.py

--- intro_2_python/step5_prog_input.py
+++ intro_2_python/step5_prog_input.py
@@ -1,3 +1,10 @@
+#srbridge.com
+#https://github.com/srbridgecom
+#https://www.facebook.com/srbridge
+#https://www.linkedin.com/in/r-bridge-3baa332a4/
+#Python
+
+#PROGRAM to accept input
 #Using input to grab data, if data display
 #prog will pause and ask for input, then print out line by line
 #if you enter no name, get yelled at and exit
