5005d11a44c0a93eaedc442cbfa350e603a66ce5
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2024-05-16 10:13:29 -0700

    Update step3.py

--- intro_2_python/step3.py
+++ intro_2_python/step3.py
@@ -1,3 +1,9 @@
+#srbridge.com
+#https://github.com/srbridgecom
+#https://www.facebook.com/srbridge
+#https://www.linkedin.com/in/r-bridge-3baa332a4/
+#Python
+
 #Name return value from a funtion
 def welcome(name, location):
     print("Hi", name, "welcome to", location)
