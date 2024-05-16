059b9771e2e9cf53f6e9413cc8aa06ced9f4a239
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2024-05-16 10:22:37 -0700

    Update step6_test_create_infinite_loop.py

--- intro_2_python/step6_test_create_infinite_loop.py
+++ intro_2_python/step6_test_create_infinite_loop.py
@@ -1,4 +1,9 @@
-#quick recap test\, create an infinite loop that keeps asking for names,
+#srbridge.com
+#https://github.com/srbridgecom
+#https://www.facebook.com/srbridge
+#https://www.linkedin.com/in/r-bridge-3baa332a4/
+#Python
+#quick recap test, create an infinite loop that keeps asking for names,
 #keeps greeting us using the entered name
 def say_hi(name):
     if name == '':
@@ -12,3 +17,17 @@ while True:  #this creates the loop, delete this and the prog ends after 1 name
     name = input('Hi, enter your name:')
 
     say_hi(name)
+
+
+def display_name(name):
+    if name == '':
+        print("theres no name displayed!")
+    else:
+        print('WASSUP')
+        for letter in name:
+            print(letter)
+
+while True:
+    name = input('hey, whats your name')
+
+    display_name(name)
